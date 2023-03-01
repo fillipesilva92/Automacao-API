4f7b0b3043a96fdd45f32ca30ec994b264f3b394
fillipesilva92 <f.fillipesilva@gmail.com>
2023-03-01 10:20:57 -0300

    Automacao API dados abertos

---
+++ automacao.api.dados-abertos/src/test/java/validacoes/ValidarNomeEleitoral.java
@@ -0,0 +1,22 @@
+package validacoes;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class ValidarNomeEleitoral {
+	
+	Response response;
+
+	@Test
+	public void validarNome() {
+		response = RestAssured.get("https://dadosabertos.camara.leg.br/api/v2/deputados/220593");
+		String nome = response.getBody().path("dados.ultimoStatus.nomeEleitoral", "nome eleitoral do deputado");
+		assertTrue(nome.contains("Abilio Brunini"));
+		System.out.println(nome);
+	}
+
+}
